c110309771df595ba74fd91150f5179688a62d2c
joshsylvia-eng <joshsylvia@yahoo.com>
2020-03-05 19:23:00 -0500

    new changes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # node-app
 
+npm install -g @angular/cli
+
 node https_server.js &
--- https_server.js
+++ https_server.js
@@ -1,13 +1,34 @@
-const https = require('https');
-const fs = require('fs');
+
+var https = require('https');
+var fs = require('fs');
 
 const options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
+  key: fs.readFileSync('certs/key.pem'),
+  cert: fs.readFileSync('certs/cert.pem')
 };
 
-https.createServer(options, function (req, res) {
-  res.writeHead(200);
-  res.end("hello world\n");
-}).listen(8000);
+var server = https.createServer(options, function (req, resp) {
+    
+    if (req.url === "/") {
+        fs.readFile("head-nav/head-nav.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+             
+            resp.end();
+        });
+    } else {
+        
+        resp.writeHead(200, { 'Content-Type': 'text/html' });
+        resp.write('<h1>Product Manaager</h1><br /><br />To create product please enter: ' + req.url);
+        resp.end();
+    }
+});
 
+server.listen(8000);
+ 
+console.log('Server Started listening on 8000');
